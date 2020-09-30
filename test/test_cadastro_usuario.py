c639119e2b0e697436a63c83ade69bae6847026b
Hariel Santana <hariol.santana138@gmail.com>
2020-09-30 02:19:14 -0300

    teste

--- test/test_cadastro_usuario.py
+++ test/test_cadastro_usuario.py
@@ -1,14 +1,20 @@
 import unittest
-from model.usuario import User
+from model import User
 from cadastroUsuario import CadastroUsuario
 
 class NewTest(unittest.TestCase):
     
-    def test_cadastro1(self):
-        pessoa = User(10,'João', '1191492-3231')
-        lista = []
-        if(len(lista) == 1):
-            self.assertEqual()
+    # def test_cadastro1(self):
+    #     newList = []
+    #     newUser = User(1,'Ruan', '(11)91111-1111')
+    #     register_new_user = CadastroUsuario()
+    #     register_new_user.add(newUser, newList)
+
+    #     if(len(newList) == 1):
+    #         pass
+    #     else:
+    #         EnvironmentError
+        
 
     def test_cadastro2(self):
         pass
